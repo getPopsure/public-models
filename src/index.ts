99fe6e8dbca49c70b7ee7c8b93f56edf804461ee
Seungha <kseha1080@gmail.com>
2021-04-20 10:08:12 +0200

    [NoTicket] Add country flags and mapping

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export { Gender } from './gender';
 export { LegacyGender } from './gender';
 export { CalendarDate } from './calendarDate';
 export { Address } from './address';
-export { Country, allCountries } from './country';
+export { Country, allCountries, mapCountryFlag } from './country';
 export { isPartialAddressValid } from './utils/address';
 export { countryNameFromAlphaCode, isEEACountry } from './utils/country';
