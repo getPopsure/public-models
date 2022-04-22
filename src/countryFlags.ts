6d4a6a8d36c0c85dc76d1ad4ab352179a31b1a3b
keremciu <kerem@ritmix.org>
2022-04-22 16:51:36 +0200

    feature(countries): improve country list to support multiple languages

--- src/countryFlags.ts
+++ src/countryFlags.ts
@@ -7,6 +7,8 @@ import aiFlag from './static/flags/ai.svg';
 import agFlag from './static/flags/ag.svg';
 import arFlag from './static/flags/ar.svg';
 import amFlag from './static/flags/am.svg';
+import aqFlag from './static/flags/aq.svg';
+import awFlag from './static/flags/aw.svg'
 import auFlag from './static/flags/au.svg';
 import atFlag from './static/flags/at.svg';
 import azFlag from './static/flags/az.svg';
@@ -198,418 +200,11 @@ import zmFlag from './static/flags/zm.svg';
 import zwFlag from './static/flags/zw.svg';
 import xkFlag from './static/flags/xk.svg';
 import ssFlag from './static/flags/ss.svg';
+import asFlag from './static/flags/as.svg';
 
-export type CountryCode =
-  | 'AF'
-  | 'AL'
-  | 'DZ'
-  | 'AD'
-  | 'AO'
-  | 'AI'
-  | 'AG'
-  | 'AR'
-  | 'AM'
-  | 'AU'
-  | 'AT'
-  | 'AZ'
-  | 'BS'
-  | 'BH'
-  | 'BD'
-  | 'BB'
-  | 'BY'
-  | 'BE'
-  | 'BZ'
-  | 'BJ'
-  | 'BT'
-  | 'BO'
-  | 'BA'
-  | 'BW'
-  | 'BR'
-  | 'BN'
-  | 'BG'
-  | 'BF'
-  | 'BI'
-  | 'KH'
-  | 'CM'
-  | 'CA'
-  | 'CV'
-  | 'CF'
-  | 'TD'
-  | 'CL'
-  | 'CN'
-  | 'CO'
-  | 'KM'
-  | 'CG'
-  | 'CD'
-  | 'CR'
-  | 'CI'
-  | 'HR'
-  | 'CU'
-  | 'CY'
-  | 'CZ'
-  | 'DK'
-  | 'DJ'
-  | 'DM'
-  | 'DO'
-  | 'EC'
-  | 'EG'
-  | 'SV'
-  | 'GQ'
-  | 'ER'
-  | 'EE'
-  | 'ET'
-  | 'FJ'
-  | 'FI'
-  | 'FR'
-  | 'GA'
-  | 'GM'
-  | 'GE'
-  | 'DE'
-  | 'GH'
-  | 'GR'
-  | 'GD'
-  | 'GT'
-  | 'GN'
-  | 'GW'
-  | 'GY'
-  | 'HT'
-  | 'VA'
-  | 'HN'
-  | 'HK'
-  | 'HU'
-  | 'IS'
-  | 'IN'
-  | 'ID'
-  | 'IR'
-  | 'IQ'
-  | 'IE'
-  | 'IL'
-  | 'IT'
-  | 'JM'
-  | 'JP'
-  | 'JO'
-  | 'KZ'
-  | 'KE'
-  | 'KI'
-  | 'KP'
-  | 'KR'
-  | 'KW'
-  | 'KG'
-  | 'LA'
-  | 'LV'
-  | 'LB'
-  | 'LS'
-  | 'LR'
-  | 'LY'
-  | 'LI'
-  | 'LT'
-  | 'LU'
-  | 'MK'
-  | 'MG'
-  | 'MW'
-  | 'MY'
-  | 'MV'
-  | 'ML'
-  | 'MT'
-  | 'MH'
-  | 'MR'
-  | 'MU'
-  | 'MX'
-  | 'FM'
-  | 'MD'
-  | 'MC'
-  | 'MN'
-  | 'ME'
-  | 'MA'
-  | 'MZ'
-  | 'MM'
-  | 'NA'
-  | 'NR'
-  | 'NP'
-  | 'NL'
-  | 'NZ'
-  | 'NI'
-  | 'NE'
-  | 'NG'
-  | 'NO'
-  | 'OM'
-  | 'PK'
-  | 'PW'
-  | 'PS'
-  | 'PA'
-  | 'PG'
-  | 'PY'
-  | 'PE'
-  | 'PH'
-  | 'PL'
-  | 'PT'
-  | 'PR'
-  | 'QA'
-  | 'RO'
-  | 'RU'
-  | 'RW'
-  | 'KN'
-  | 'LC'
-  | 'VC'
-  | 'WS'
-  | 'SM'
-  | 'ST'
-  | 'SA'
-  | 'SN'
-  | 'RS'
-  | 'SC'
-  | 'SL'
-  | 'SG'
-  | 'SK'
-  | 'SI'
-  | 'SB'
-  | 'SO'
-  | 'ZA'
-  | 'ES'
-  | 'LK'
-  | 'SD'
-  | 'SR'
-  | 'SZ'
-  | 'SE'
-  | 'CH'
-  | 'SY'
-  | 'TW'
-  | 'TJ'
-  | 'TZ'
-  | 'TH'
-  | 'TL'
-  | 'TG'
-  | 'TO'
-  | 'TT'
-  | 'TN'
-  | 'TR'
-  | 'TM'
-  | 'TV'
-  | 'UG'
-  | 'UA'
-  | 'AE'
-  | 'GB'
-  | 'US'
-  | 'UY'
-  | 'UZ'
-  | 'VU'
-  | 'VE'
-  | 'VN'
-  | 'YE'
-  | 'ZM'
-  | 'ZW'
-  | 'XK'
-  | 'SS';
+import { Alpha2CountryCode } from './countryCodes';
 
-export interface Country {
-  name: string;
-  code: CountryCode;
-}
-
-export const allCountries: Country[] = [
-  { name: 'Afghanistan', code: 'AF' },
-  { name: 'Albania', code: 'AL' },
-  { name: 'Algeria', code: 'DZ' },
-  { name: 'Andorra', code: 'AD' },
-  { name: 'Angola', code: 'AO' },
-  { name: 'Anguilla', code: 'AI' },
-  { name: 'Antigua and Barbuda', code: 'AG' },
-  { name: 'Argentina', code: 'AR' },
-  { name: 'Armenia', code: 'AM' },
-  { name: 'Australia', code: 'AU' },
-  { name: 'Austria', code: 'AT' },
-  { name: 'Azerbaijan', code: 'AZ' },
-  { name: 'Bahamas', code: 'BS' },
-  { name: 'Bahrain', code: 'BH' },
-  { name: 'Bangladesh', code: 'BD' },
-  { name: 'Barbados', code: 'BB' },
-  { name: 'Belarus', code: 'BY' },
-  { name: 'Belgium', code: 'BE' },
-  { name: 'Belize', code: 'BZ' },
-  { name: 'Benin', code: 'BJ' },
-  { name: 'Bhutan', code: 'BT' },
-  { name: 'Bolivia', code: 'BO' },
-  { name: 'Bosnia and Herzegovina', code: 'BA' },
-  { name: 'Botswana', code: 'BW' },
-  { name: 'Brazil', code: 'BR' },
-  { name: 'Brunei Darussalam', code: 'BN' },
-  { name: 'Bulgaria', code: 'BG' },
-  { name: 'Burkina Faso', code: 'BF' },
-  { name: 'Burundi', code: 'BI' },
-  { name: 'Cambodia', code: 'KH' },
-  { name: 'Cameroon', code: 'CM' },
-  { name: 'Canada', code: 'CA' },
-  { name: 'Cape Verde', code: 'CV' },
-  { name: 'Central African Republic', code: 'CF' },
-  { name: 'Chad', code: 'TD' },
-  { name: 'Chile', code: 'CL' },
-  { name: 'China', code: 'CN' },
-  { name: 'Colombia', code: 'CO' },
-  { name: 'Comoros', code: 'KM' },
-  { name: 'Congo, Republic of the', code: 'CG' },
-  { name: 'Congo, The Democratic Republic of the', code: 'CD' },
-  { name: 'Costa Rica', code: 'CR' },
-  { name: 'Cote D’Ivoire', code: 'CI' },
-  { name: 'Croatia', code: 'HR' },
-  { name: 'Cuba', code: 'CU' },
-  { name: 'Cyprus', code: 'CY' },
-  { name: 'Czech Republic', code: 'CZ' },
-  { name: 'Denmark', code: 'DK' },
-  { name: 'Djibouti', code: 'DJ' },
-  { name: 'Dominica', code: 'DM' },
-  { name: 'Dominican Republic', code: 'DO' },
-  { name: 'Ecuador', code: 'EC' },
-  { name: 'Egypt', code: 'EG' },
-  { name: 'El Salvador', code: 'SV' },
-  { name: 'Equatorial Guinea', code: 'GQ' },
-  { name: 'Eritrea', code: 'ER' },
-  { name: 'Estonia', code: 'EE' },
-  { name: 'Ethiopia', code: 'ET' },
-  { name: 'Fiji', code: 'FJ' },
-  { name: 'Finland', code: 'FI' },
-  { name: 'France', code: 'FR' },
-  { name: 'Gabon', code: 'GA' },
-  { name: 'Gambia', code: 'GM' },
-  { name: 'Georgia', code: 'GE' },
-  { name: 'Germany', code: 'DE' },
-  { name: 'Ghana', code: 'GH' },
-  { name: 'Greece', code: 'GR' },
-  { name: 'Grenada', code: 'GD' },
-  { name: 'Guatemala', code: 'GT' },
-  { name: 'Guinea', code: 'GN' },
-  { name: 'Guinea-Bissau', code: 'GW' },
-  { name: 'Guyana', code: 'GY' },
-  { name: 'Haiti', code: 'HT' },
-  { name: 'Holy See (Vatican City State)', code: 'VA' },
-  { name: 'Honduras', code: 'HN' },
-  { name: 'Hong Kong', code: 'HK' },
-  { name: 'Hungary', code: 'HU' },
-  { name: 'Iceland', code: 'IS' },
-  { name: 'India', code: 'IN' },
-  { name: 'Indonesia', code: 'ID' },
-  { name: 'Iran, Islamic Republic Of', code: 'IR' },
-  { name: 'Iraq', code: 'IQ' },
-  { name: 'Ireland', code: 'IE' },
-  { name: 'Israel', code: 'IL' },
-  { name: 'Italy', code: 'IT' },
-  { name: 'Jamaica', code: 'JM' },
-  { name: 'Japan', code: 'JP' },
-  { name: 'Jordan', code: 'JO' },
-  { name: 'Kazakhstan', code: 'KZ' },
-  { name: 'Kenya', code: 'KE' },
-  { name: 'Kiribati', code: 'KI' },
-  { name: 'Korea, Democratic People’s Republic of', code: 'KP' },
-  { name: 'Korea, Republic of', code: 'KR' },
-  { name: 'Kosovo', code: 'XK' },
-  { name: 'Kuwait', code: 'KW' },
-  { name: 'Kyrgyzstan', code: 'KG' },
-  { name: 'Lao People’s Democratic Republic', code: 'LA' },
-  { name: 'Latvia', code: 'LV' },
-  { name: 'Lebanon', code: 'LB' },
-  { name: 'Lesotho', code: 'LS' },
-  { name: 'Liberia', code: 'LR' },
-  { name: 'Libya', code: 'LY' },
-  { name: 'Liechtenstein', code: 'LI' },
-  { name: 'Lithuania', code: 'LT' },
-  { name: 'Luxembourg', code: 'LU' },
-  { name: 'North Macedonia, Republic of', code: 'MK' },
-  { name: 'Madagascar', code: 'MG' },
-  { name: 'Malawi', code: 'MW' },
-  { name: 'Malaysia', code: 'MY' },
-  { name: 'Maldives', code: 'MV' },
-  { name: 'Mali', code: 'ML' },
-  { name: 'Malta', code: 'MT' },
-  { name: 'Marshall Islands', code: 'MH' },
-  { name: 'Mauritania', code: 'MR' },
-  { name: 'Mauritius', code: 'MU' },
-  { name: 'Mexico', code: 'MX' },
-  { name: 'Micronesia, Federated States of', code: 'FM' },
-  { name: 'Moldova, Republic of', code: 'MD' },
-  { name: 'Monaco', code: 'MC' },
-  { name: 'Mongolia', code: 'MN' },
-  { name: 'Montenegro', code: 'ME' },
-  { name: 'Morocco', code: 'MA' },
-  { name: 'Mozambique', code: 'MZ' },
-  { name: 'Myanmar', code: 'MM' },
-  { name: 'Namibia', code: 'NA' },
-  { name: 'Nauru', code: 'NR' },
-  { name: 'Nepal', code: 'NP' },
-  { name: 'Netherlands', code: 'NL' },
-  { name: 'New Zealand', code: 'NZ' },
-  { name: 'Nicaragua', code: 'NI' },
-  { name: 'Niger', code: 'NE' },
-  { name: 'Nigeria', code: 'NG' },
-  { name: 'Norway', code: 'NO' },
-  { name: 'Oman', code: 'OM' },
-  { name: 'Pakistan', code: 'PK' },
-  { name: 'Palau', code: 'PW' },
-  { name: 'Palestine', code: 'PS' },
-  { name: 'Panama', code: 'PA' },
-  { name: 'Papua New Guinea', code: 'PG' },
-  { name: 'Paraguay', code: 'PY' },
-  { name: 'Peru', code: 'PE' },
-  { name: 'Philippines', code: 'PH' },
-  { name: 'Poland', code: 'PL' },
-  { name: 'Portugal', code: 'PT' },
-  { name: 'Puerto Rico', code: 'PR' },
-  { name: 'Qatar', code: 'QA' },
-  { name: 'Romania', code: 'RO' },
-  { name: 'Russian Federation', code: 'RU' },
-  { name: 'Rwanda', code: 'RW' },
-  { name: 'Saint Kitts and Nevis', code: 'KN' },
-  { name: 'Saint Lucia', code: 'LC' },
-  { name: 'Saint Vincent and the Grenadines', code: 'VC' },
-  { name: 'Samoa', code: 'WS' },
-  { name: 'San Marino', code: 'SM' },
-  { name: 'Sao Tome and Principe', code: 'ST' },
-  { name: 'Saudi Arabia', code: 'SA' },
-  { name: 'Senegal', code: 'SN' },
-  { name: 'Serbia', code: 'RS' },
-  { name: 'Seychelles', code: 'SC' },
-  { name: 'Sierra Leone', code: 'SL' },
-  { name: 'Singapore', code: 'SG' },
-  { name: 'Slovakia', code: 'SK' },
-  { name: 'Slovenia', code: 'SI' },
-  { name: 'Solomon Islands', code: 'SB' },
-  { name: 'Somalia', code: 'SO' },
-  { name: 'South Africa', code: 'ZA' },
-  { name: 'Spain', code: 'ES' },
-  { name: 'Sri Lanka', code: 'LK' },
-  { name: 'Sudan', code: 'SD' },
-  { name: 'South Sudan, Republic of', code: 'SS' },
-  { name: 'Suriname', code: 'SR' },
-  { name: 'Eswatini', code: 'SZ' },
-  { name: 'Sweden', code: 'SE' },
-  { name: 'Switzerland', code: 'CH' },
-  { name: 'Syrian Arab Republic', code: 'SY' },
-  { name: 'Taiwan', code: 'TW' },
-  { name: 'Tajikistan', code: 'TJ' },
-  { name: 'Tanzania, United Republic of', code: 'TZ' },
-  { name: 'Thailand', code: 'TH' },
-  { name: 'Timor-Leste', code: 'TL' },
-  { name: 'Togo', code: 'TG' },
-  { name: 'Tonga', code: 'TO' },
-  { name: 'Trinidad and Tobago', code: 'TT' },
-  { name: 'Tunisia', code: 'TN' },
-  { name: 'Turkey', code: 'TR' },
-  { name: 'Turkmenistan', code: 'TM' },
-  { name: 'Tuvalu', code: 'TV' },
-  { name: 'Uganda', code: 'UG' },
-  { name: 'Ukraine', code: 'UA' },
-  { name: 'United Arab Emirates', code: 'AE' },
-  { name: 'United Kingdom', code: 'GB' },
-  { name: 'United States', code: 'US' },
-  { name: 'Uruguay', code: 'UY' },
-  { name: 'Uzbekistan', code: 'UZ' },
-  { name: 'Vanuatu', code: 'VU' },
-  { name: 'Venezuela', code: 'VE' },
-  { name: 'Vietnam', code: 'VN' },
-  { name: 'Yemen', code: 'YE' },
-  { name: 'Zambia', code: 'ZM' },
-  { name: 'Zimbabwe', code: 'ZW' },
-];
-
-export const mapCountryFlag: { [K in CountryCode]: string } = {
+export const mapCountryFlag: { [K in Alpha2CountryCode]: string } = {
   AF: afFlag,
   AL: alFlag,
   DZ: dzFlag,
@@ -619,6 +214,7 @@ export const mapCountryFlag: { [K in CountryCode]: string } = {
   AG: agFlag,
   AR: arFlag,
   AM: amFlag,
+  AW: awFlag,
   AU: auFlag,
   AT: atFlag,
   AZ: azFlag,
@@ -808,6 +404,55 @@ export const mapCountryFlag: { [K in CountryCode]: string } = {
   YE: yeFlag,
   ZM: zmFlag,
   ZW: zwFlag,
+  BL: frFlag,
+  MF: frFlag,
+  SX: nlFlag,
+  JE: gbFlag,
+  IM: gbFlag,
+  GG: gbFlag,
+  CW: nlFlag,
+  BQ: nlFlag,
+  AX: fiFlag,
+  EH: maFlag,
+  WF: frFlag,
+  VI: usFlag,
+  VG: gbFlag,
+  UM: usFlag,
+  TC: gbFlag,
+  TK: nzFlag,
+  SJ: noFlag,
+  GS: gbFlag,
+  PM: frFlag,
+  SH: gbFlag,
+  RE: frFlag,
+  PN: gbFlag,
+  MP: usFlag,
+  NF: auFlag,
+  NU: gbFlag,
+  NC: frFlag,
+  MS: gbFlag,
+  MQ: frFlag,
+  MO: cnFlag,
+  HM: auFlag,
+  GU: usFlag,
+  GP: frFlag,
+  GL: dkFlag,
+  GI: gbFlag,
+  TF: frFlag,
+  PF: frFlag,
+  GF: frFlag,
+  FO: dkFlag,
+  FK: gbFlag,
+  CK: gbFlag,
+  CC: auFlag,
+  CX: auFlag,
+  KY: gbFlag,
+  IO: gbFlag,
+  BV: noFlag,
+  BM: gbFlag,
   XK: xkFlag,
   SS: ssFlag,
+  AQ: aqFlag,
+  AS: asFlag,
+  YT: frFlag,
 };
