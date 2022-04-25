1be7ae920616353eb4e6ae54b920bccd7d73887a
Kerem Sevencan <kerem@ritmix.org>
2022-04-25 09:55:39 +0200

    Import rollup/json (#29)
    * feature(semver): version bump
    
    * revert(semver): package version

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript';
 import image from '@rollup/plugin-image';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/index.ts',
@@ -9,5 +10,5 @@ export default {
     format: 'cjs',
     sourcemap: true,
   },
-  plugins: [typescript(), image()],
+  plugins: [typescript(), image(), json()],
 };
