608fe7a34d52f162603d6dce9dec0360f6e5071c
Kerem Sevencan <kerem@ritmix.org>
2022-04-27 13:01:12 +0200

    Kerem/no ticket/update version and add test (#30)
    * v1.0.0-alpha
    
    * test(countrylist): cover getAllCountries function in test
    
    * package version 1.0.0

--- src/utils/country/index.test.ts
+++ src/utils/country/index.test.ts
@@ -1,4 +1,4 @@
-import { countryNameFromAlphaCode, isEEACountry } from '.';
+import { countryNameFromAlphaCode, getAllCountries, isEEACountry } from '.';
 
 describe('Countries with alpha code', () => {
   it('Should find country name for "DE" alpha code', () => {
@@ -7,6 +7,51 @@ describe('Countries with alpha code', () => {
 
   it('Should find country name for "DE" alpha code with german translation', () => {
     expect(countryNameFromAlphaCode('DE', 'de')).toEqual('Deutschland');
+  });
+});
+
+describe('getAllCountries', () => {
+  it('Should return all countries', () => {
+    const countries = getAllCountries();
+    expect(countries.length).toEqual(250);
+  });
+
+  it('Should return all countries with german translation', () => {
+    const countries = getAllCountries('de');
+    expect(countries).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          code: 'DE',
+          name: 'Deutschland',
+        }),
+      ])
+    );
+  });
+
+  describe('when no language defined', () => {
+    it('Should return all countries with english translation', () => {
+      const countries = getAllCountries();
+      expect(countries).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            code: 'DE',
+            name: 'Germany',
+          }),
+        ])
+      );
+    });
+  });
+
+  it('Should return all countries with english translation', () => {
+    const countries = getAllCountries('en');
+    expect(countries).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          code: 'DE',
+          name: 'Germany',
+        }),
+      ])
+    );
   });
 });
 
