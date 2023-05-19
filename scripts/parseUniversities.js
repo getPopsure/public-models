aafff1e578d3d063ab158a3c058cd56495d876b0
Diogo Mateus <diogoamateus@gmail.com>
2023-05-19 10:27:23 +0100

    feat(universities-list): [EMU-5786] Add script to update universities list (#45)
    * feat(universities-list): [EMU-5786] Add script to update universities list
    
    * Fix typo

---
+++ scripts/parseUniversities.js
@@ -0,0 +1,46 @@
+
+const fs = require('fs');
+const { parse } = require('csv-parse');
+
+const filePath = process.argv.slice(2)?.[0];
+
+if (!filePath) {
+  console.error('Please provide the filepath as an argument')
+}
+
+const rs = fs.createReadStream(process.argv.slice(2)?.[0]);
+
+const parser = parse({
+  delimiter: "\t",
+  columns: true,
+}, (error, data) => {
+  if (error) {
+    console.log('error :>> ', error);
+    
+    return;
+  }
+
+  const writeStream = fs.createWriteStream('src/universities.ts');
+  const pathName = writeStream.path;
+
+  // write each value of the array on the file
+  writeStream.write('// This is an auto-generated file, pleased don\'t update it manually\n');
+  writeStream.write('export const universities = [\n');
+  data.sort().map(({ Hochschulname }) => writeStream.write(`  '${Hochschulname}',\n`));
+  writeStream.write('];\n');
+
+  // the finish event is emitted when all data has been flushed from the stream
+  writeStream.on('finish', () => {
+    console.log(`Successfully rewrote ${pathName} file with updated data`);
+  });
+
+  // handle the errors on the write process
+  writeStream.on('error', (err) => {
+    console.error(`There is an error writing the file ${pathName} => ${err}`)
+  });
+
+  // close the stream
+  writeStream.end();
+});
+
+rs.pipe(parser);
