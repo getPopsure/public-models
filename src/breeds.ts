63a982a489c21d0c6875b90fbf558acad0e16ba1
busypeoples <busypeoples@gmail.com>
2023-05-31 13:33:21 +0200

    feat(breeds): [NoTicket] add blocked dog breeds list (#47)

---
+++ src/breeds.ts
@@ -0,0 +1,18 @@
+export const dangerousBreeds = [
+  'American Pit Bull Terrier',
+  'American Straffordshire Terrier',
+  'Bullmastiff',
+  'Bullterrier (any kind)',
+  'Dogo Argentino',
+  'Dogue de Bordeaux',
+  'Fila Brasileiro',
+  'Mastiff',
+  'Mastín Español',
+  'Mastino Napoletano',
+  'Pit Bull Terrier',
+  'Rottweiler',
+  'Staffordshire Bullterrier',
+  'Tosa Inu',
+];
+
+export type DangerousBreeds = typeof dangerousBreeds[number];
